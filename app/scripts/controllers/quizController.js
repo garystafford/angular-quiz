5f895c8a5c4a065f7b93ffe3c16f1c790d66f195
Gary A. Stafford <garystafford@rochester.rr.com>
2014-06-15 23:50:37 -0400

    Added filter to controller for question Id.

--- app/scripts/controllers/quizController.js
+++ app/scripts/controllers/quizController.js
@@ -8,9 +8,14 @@
  * Controller of the angulardataApp
  */
 angular.module('angulardataApp')
-  .controller('QuizCtrl', function ($scope, quizFactory) {
+  .controller('QuizCtrl', function ($scope, quizFactory, filterFilter) {
     $scope.quiz = {};
+
     quizFactory.get(function (data) {
       $scope.quiz = data.questions;
     });
+
+    $scope.filteredQuestion = function(questionId) {
+      return filterFilter($scope.quiz, {_id: questionId});
+    }
   });
