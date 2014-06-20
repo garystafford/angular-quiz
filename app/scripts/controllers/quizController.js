9ca6deda59556360092eff2b8b4f138198da07b1
Gary A. Stafford <garystafford@rochester.rr.com>
2014-06-20 11:05:57 -0400

    Made response to answer display using ng-if. Working.

--- app/scripts/controllers/quizController.js
+++ app/scripts/controllers/quizController.js
@@ -10,23 +10,30 @@
 angular.module('angulardataApp')
   .controller('QuizCtrl', function ($scope, quizFactory, filterFilter) {
     $scope.quiz = {};
+    $scope.results = [];
 
     quizFactory.get(function (data) {
       $scope.quiz = data.questions;
+      var len = $scope.quiz.length;
+      for (var i = 0; i < len; i++) {
+        $scope.results.push({
+          _id:        $scope.quiz[i]._id,
+          answer:     $scope.quiz[i].answer,
+          userChoice: null,
+          correct:    null
+        });
+      }
     });
 
     $scope.responseMultipleChoice = "";
     $scope.color = 'red';
-    $scope.fontWeight = 'normal';
-    $scope.checkMultipleChoice = function (answer, userChoice) {
-      if (answer == userChoice) {
-        $scope.responseMultipleChoice = "Correct!";
-        $scope.color = 'seagreen';
-        $scope.fontWeight = 'bold';
+    $scope.checkMultipleChoice = function (question, userChoice) {
+      $scope.results[question - 1].userChoice = userChoice;
+
+      if ($scope.results[question - 1].answer == userChoice) {
+        $scope.results[question - 1].correct = true;
       } else {
-        $scope.responseMultipleChoice = "Incorrect";
-        $scope.color = 'red';
-        $scope.fontWeight = 'normal';
+        $scope.results[question - 1].correct = false;
       }
     };
 
